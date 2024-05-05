71dda41c7554c23dc1a8dd6eb0674a7e633dc2d1
Nochi <48821257+ainochi-kor@users.noreply.github.com>
2024-05-05 17:01:21 +0900

    feat: Summary Form (#1)
    * test: SummaryForm
    
    * feat: SummaryForm

---
+++ src/pages/summary/SummaryForm.tsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import Popover from "react-bootstrap/Popover";
+
+interface SummaryFormProps {
+  setOrderPhase: (phase: string) => void;
+}
+
+const SummaryForm: React.FC<SummaryFormProps> = ({ setOrderPhase }) => {
+  const [tcChecked, setTcChecked] = useState(false);
+
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    // pass along to the next phase.
+    // The next page will handle submitting order from context.
+    setOrderPhase("completed");
+  }
+
+  const popover = (
+    <Popover id="popover-basic">
+      <Popover.Body>No ice cream will actually be delivered</Popover.Body>
+    </Popover>
+  );
+
+  const checkboxLabel = (
+    <span>
+      I agree to
+      <OverlayTrigger placement="right" overlay={popover}>
+        <span style={{ color: "blue" }}> Terms and Conditions</span>
+      </OverlayTrigger>
+    </span>
+  );
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Form.Group controlId="terms-and-conditions">
+        <Form.Check
+          type="checkbox"
+          checked={tcChecked}
+          onChange={(e) => setTcChecked(e.target.checked)}
+          label={checkboxLabel}
+        />
+      </Form.Group>
+      <Button variant="primary" type="submit" disabled={!tcChecked}>
+        Confirm order
+      </Button>
+    </Form>
+  );
+};
+
+export default SummaryForm;
